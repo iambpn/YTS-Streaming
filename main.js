504a496f0eae517a3d4f487554b6bf9ceef8cc74
Bipin Maharjan <bopon.maharjan@gmail.com>
2020-07-26 13:04:31 +0545

    minor bug fix

--- main.js
+++ main.js
@@ -194,7 +194,7 @@ function createWindow() {
     mainWindow.loadFile('main.html')
 
     // Open DevTools - Remove for PRODUCTION!
-    mainWindow.webContents.openDevTools()
+    //mainWindow.webContents.openDevTools()
 
     // Listen for window being closed
     mainWindow.on('closed', () => {
