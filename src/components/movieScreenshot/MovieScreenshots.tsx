d4d3c12a73ab8f966650cb431469c3b9bf59c16e
Bipin Maharjan <21078512+iambpn@users.noreply.github.com>
2021-06-14 16:35:52 +0545

    Create MovieScreenshots.tsx

---
+++ src/components/movieScreenshot/MovieScreenshots.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+type MovieScreenshotsProps = {
+    title_long: string;
+    medium_screenshot_image1: string;
+    medium_screenshot_image2: string;
+    medium_screenshot_image3: string;
+}
+
+export default function MovieScreenshots(props:MovieScreenshotsProps){
+    return(
+        <div className="row mt-5 justify-content-center">
+            <div className="col-4">
+                <img src={props.medium_screenshot_image1} alt={props.title_long} className="img-fluid"/>
+            </div>
+            <div className="col-4">
+                <img src={props.medium_screenshot_image2} alt={props.title_long} className="img-fluid"/>
+            </div>
+            <div className="col-4">
+                <img src={props.medium_screenshot_image3} alt={props.title_long} className="img-fluid"/>
+            </div>
+        </div>
+    );
+}
