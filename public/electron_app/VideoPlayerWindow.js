97cff3bfcbae0b7ccbb453de0b8a18beaa437a03
Bipin Maharjan <21078512+iambpn@users.noreply.github.com>
2021-06-17 19:35:27 +0545

    Create VideoPlayerWindow.js

---
+++ public/electron_app/VideoPlayerWindow.js
@@ -0,0 +1,41 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const electron_1 = require("electron");
+class VideoPlayerWindow extends electron_1.BrowserWindow {
+    constructor(url, parentWindow) {
+        super({
+            width: 1000,
+            height: 600,
+            minWidth: 1000,
+            minHeight: 600,
+            darkTheme: true,
+            backgroundColor: "#060606",
+            title: "Now Playing",
+            autoHideMenuBar: true,
+            webPreferences: {
+                enableRemoteModule: false
+            }
+        });
+        this.setMenu(electron_1.Menu.buildFromTemplate([
+            {
+                label: "About",
+                submenu: [
+                    {
+                        label: "View Shortcuts",
+                        click: () => {
+                            electron_1.shell.openExternal('https://github.com/sampotts/plyr#shortcuts');
+                        }
+                    },
+                    {
+                        label: 'Plyr Player',
+                        click: () => {
+                            electron_1.shell.openExternal("https://github.com/sampotts/plyr");
+                        },
+                    }
+                ]
+            }
+        ]));
+        this.loadURL(url);
+    }
+}
+exports.default = VideoPlayerWindow;
