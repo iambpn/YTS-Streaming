e2f2abb43615e0fa4f79eb4e0f9c2141b60a7b31
Bipin Maharjan <21078512+iambpn@users.noreply.github.com>
2021-06-19 12:34:42 +0545

    new IPC channel added

--- public/electron_app/preload.js
+++ public/electron_app/preload.js
@@ -3,13 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const electron_1 = require("electron");
 electron_1.contextBridge.exposeInMainWorld("api", {
     send: (channel, data) => {
-        let allow_channels = ["ExternalLink:Open", "Cache:ClearCache", "Cache:ShowSpaceRequest", "video:play"];
+        let allow_channels = ["ExternalLink:Open", "Cache:ClearCache", "Cache:ShowSpaceRequest", "video:play",
+            "download:stop", "download:pause", "download:resume"];
         if (allow_channels.includes(channel)) {
             electron_1.ipcRenderer.send(channel, data);
         }
     },
     receive: (channel, func) => {
-        let allow_channels = ["Cache:ShowSpaceResponse"];
+        let allow_channels = ["Cache:ShowSpaceResponse", "download:info"];
         if (allow_channels.includes(channel)) {
             electron_1.ipcRenderer.on(channel, (e, ...args) => func(...args));
         }
--- public/electron_app/preload.ts
+++ public/electron_app/preload.ts
@@ -3,13 +3,14 @@ import {ipcRenderer, contextBridge} from "electron";
 contextBridge.exposeInMainWorld(
     "api", {
         send: (channel: string, data: any) => {
-            let allow_channels = ["ExternalLink:Open","Cache:ClearCache","Cache:ShowSpaceRequest","video:play"];
+            let allow_channels = ["ExternalLink:Open","Cache:ClearCache","Cache:ShowSpaceRequest","video:play",
+            "download:stop","download:pause", "download:resume"];
             if (allow_channels.includes(channel)) {
                 ipcRenderer.send(channel, data);
             }
         },
         receive: (channel: string, func: (...data: any) => void) => {
-            let allow_channels = ["Cache:ShowSpaceResponse"];
+            let allow_channels = ["Cache:ShowSpaceResponse","download:info"];
             if (allow_channels.includes(channel)) {
                 ipcRenderer.on(channel, (e, ...args) => func(...args));
             }
