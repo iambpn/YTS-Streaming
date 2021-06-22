14917d51d4f7a338211edf705e0b1deb1e27fd10
Bipin Maharjan <21078512+iambpn@users.noreply.github.com>
2021-06-22 17:49:53 +0545

    Changing IMG url

--- src/components/spinner/Spinner.tsx
+++ src/components/spinner/Spinner.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Spinner() {
     return (
         <div className={"d-flex justify-content-center"}>
-            <img src="/public_assets/images/preloader.gif" alt="" className="img-fluid" width="400"
+            <img src="./public_assets/images/preloader.gif" alt="" className="img-fluid" width="400"
                  height="400"/>
         </div>
     );
