548cf5729d51abdca8fec0a292d4e313d765006b
Bipin Maharjan <21078512+iambpn@users.noreply.github.com>
2021-12-27 23:03:29 +0545

    adding invoke function for promise based ipc call.

--- public/electron_app/preload.js
+++ public/electron_app/preload.js
@@ -1,14 +1,29 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const electron_1 = require("electron");
 electron_1.contextBridge.exposeInMainWorld("api", {
     send: (channel, data) => {
-        let allow_channels = ["ExternalLink:Open", "Cache:ClearCache", "Cache:ShowSpaceRequest", "video:play",
-            "download:stop", "download:pause", "download:resume", "style:caption"];
+        let allow_channels = ["ExternalLink:Open", "Cache:ClearCache", "Cache:ShowSpaceRequest", "download:stop",
+            "download:pause", "download:resume", "style:caption"];
         if (allow_channels.includes(channel)) {
             electron_1.ipcRenderer.send(channel, data);
         }
     },
+    invoke: (channel, data) => __awaiter(void 0, void 0, void 0, function* () {
+        let allow_channels = ["video:play"];
+        if (allow_channels.includes(channel)) {
+            return yield electron_1.ipcRenderer.invoke(channel, data);
+        }
+    }),
     receive: (channel, func) => {
         let allow_channels = ["Cache:ShowSpaceResponse", "download:info", "get:style:caption"];
         if (allow_channels.includes(channel)) {
--- public/electron_app/preload.ts
+++ public/electron_app/preload.ts
@@ -3,11 +3,17 @@ import {ipcRenderer, contextBridge} from "electron";
 contextBridge.exposeInMainWorld(
     "api", {
         send: (channel: string, data: any) => {
-            let allow_channels = ["ExternalLink:Open","Cache:ClearCache","Cache:ShowSpaceRequest","video:play",
-            "download:stop","download:pause", "download:resume", "style:caption"];
+            let allow_channels = ["ExternalLink:Open","Cache:ClearCache","Cache:ShowSpaceRequest","download:stop",
+              "download:pause", "download:resume", "style:caption"];
             if (allow_channels.includes(channel)) {
                 ipcRenderer.send(channel, data);
             }
+        },
+        invoke:async(channel: string, data: any)=>{
+          let allow_channels = ["video:play"];
+          if (allow_channels.includes(channel)) {
+            return await ipcRenderer.invoke(channel, data);
+          }
         },
         receive: (channel: string, func: (...data: any) => void) => {
             let allow_channels = ["Cache:ShowSpaceResponse","download:info","get:style:caption"];
